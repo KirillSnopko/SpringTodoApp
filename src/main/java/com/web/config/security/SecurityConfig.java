d97eff36305f113af943507f5722c352105ada49
Kirill Snopko <kirillsnopko@gmail.com>
2022-02-04 18:17:33 +0300

    fe

--- src/main/java/com/web/config/security/SecurityConfig.java
+++ src/main/java/com/web/config/security/SecurityConfig.java
@@ -1,50 +1,62 @@
 package com.web.config.security;
 
+import com.web.service.MySimpleUrlAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.sql.DataSource;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-/*
-    @Autowired
-    public void configGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("kira").password("{noop}1234").roles("USER");
-    }
-*/
+    /*
+        @Autowired
+        public void configGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("kira").password("{noop}1234").roles("USER");
+        }
+    */
     @Autowired
     private DataSource dataSource;
 
-     @Autowired
-     public void configGlobal(AuthenticationManagerBuilder auth)throws Exception {
-         auth.jdbcAuthentication().dataSource(dataSource);
-             //    .usersByUsernameQuery("")
-             //    .authoritiesByUsernameQuery("");
+    @Autowired
+    public void configGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+        //    .usersByUsernameQuery("")
+        //    .authoritiesByUsernameQuery("");
 
-     }
+    }
 
+    //defaultSuccessUrl("/user")
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/user/*").hasRole("USER")
-                .antMatchers("/admin/*").hasAnyRole("ADMIN")
+                .antMatchers("/user/*", "/user").hasRole("USER")
+                .antMatchers("/admin/*", "/admin").hasAnyRole("ADMIN")
                 .antMatchers("/").permitAll()
-                .antMatchers("/login","/sign_up","/signup").anonymous()
+                .antMatchers("/login", "/sign_up", "/signup").anonymous()
                 .and().csrf().disable()
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
                 .usernameParameter("login")
+                .successHandler(myAuthenticationSuccessHandler())
                 .and()
                 .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID");
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler() {
+        return new MySimpleUrlAuthenticationSuccessHandler();
     }
 }
